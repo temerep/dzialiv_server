098af7f41e26238a399827466a6671374f25d298
System Administrator <root@MacBook-Pro.local>
2023-10-15 23:12:53 +0300

    update

--- controllers/SubcategoryController.js
+++ controllers/SubcategoryController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { default: mongoose } = require("mongoose");
 const Subcategory = require("../models/Subcategory");
